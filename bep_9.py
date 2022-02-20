de0ff9d879528fd7f2cd88fd0e50358b77f42322
J-45 <64985480+J-45@users.noreply.github.com>
2022-02-20 12:00:12 +0100

    Update bep_9.py

--- bep_9.py
+++ bep_9.py
@@ -1,8 +1,7 @@
 import bencode
 import sys
 
-# print(bencode.bdecode("d1:md11:ut_metadatai9e6:ut_pexi8ee13:metadata_sizei234481e1:pi3666e1:v12:aria2/1.35.0e"))
-# print(bencode.bdecode("d1:ei0e1:md11:ut_metadatai2e6:ut_pexi0ee13:metadata_sizei234481e1:pi51005e4:reqqi2048e1:v17:libTorrent 0.13.8e"))
+# print(bencode.bdecode("d8:msg_typei0e5:piecei0ee"))
 # sys.exit(0)
 
 import os
@@ -15,23 +14,23 @@ import bencode
 import hashlib
 import struct
 
-IP = "127.0.0.1" # socket.gethostbyname('router.bittorrent.com')
+IP = "192.168.1.28" # socket.gethostbyname('router.bittorrent.com')
 PORT = 6881 # 6881
-BUFFER = 512
+BUFFER = 4096
 CLIENT_SOCKET       = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # CLIENT_SOCKET.bind((IP, PORT))
 CLIENT_SOCKET.connect((IP,PORT))
-CLIENT_SOCKET.settimeout(7)
+CLIENT_SOCKET.settimeout(3)
 
 
-
+print("\n",'-' * 21,"HANDSHAKE",'-' * 21,"\n")
 
 # http://www.bittorrent.org/beps/bep_0010.html
 PROTOCOL_IDENTIFIER = b'BitTorrent protocol'
 msg = struct.pack('B', len(PROTOCOL_IDENTIFIER)) # struct.pack('B', 19) = b'\x13' = 19 length of 'string identifier of the protocol' below
 msg += PROTOCOL_IDENTIFIER # string identifier of the protocol
-msg += b'\x00\x00\x00\x00\x00\x10\x00\x05' # eight (8) reserved bytes
-msg += bytearray.fromhex("F1FCDC1462D36530F526C1D9402EEC9100B7BA18") # info_hash
+msg += b'\x00\x00\x00\x00\x00\x10\x00\x01' # https://www.bittorrent.org/beps/bep_0004.html
+msg += bytearray.fromhex("f1fcdc1462d36530f526c1d9402eec9100b7ba18") # info_hash
 msg += b"-J4" + b"0001" + b"-" + hashlib.sha1(str(os.environ).encode()).hexdigest()[9:20].encode() # peer_id Azureus-style
 
 print(">>>", msg)
@@ -44,14 +43,14 @@ except socket.timeout:
     print((IP, PORT),'timeout !')
 
 
-print("")
+print("\n",'-' * 21,"EXTENDED HANDSHAKE",'-' * 21,"\n")
 
 
 # http://www.bittorrent.org/beps/bep_0009.html
 BITTORRENT_MESSAGE_ID = 20
-EXTENDED_MESSAGE_ID = 0
+extended_message_id = 1
 msg = chr(BITTORRENT_MESSAGE_ID).encode() # struct.pack("B",20)
-msg += chr(EXTENDED_MESSAGE_ID).encode() # struct.pack("B",0)
+msg += chr(extended_message_id).encode() # struct.pack("B",0)
 msg += bencode.bencode({"m":{"ut_metadata":1}}).encode()
 
 LENGTH_PREFIX = struct.pack(">I",len(msg)) # len(msg).to_bytes(4, 'big') # struct.pack(">I",len(msg))
@@ -62,34 +61,38 @@ CLIENT_SOCKET.send(msg)
 try:
     server_response = CLIENT_SOCKET.recv(BUFFER)
     print("<<<",server_response)
-
-    # get metadata_size
-    regex =  re.compile(r"metadata_sizei(\d+)e")
-    matches = regex.search(server_response.decode("utf-8", "ignore"))
-    metadata_size = matches.group(1)
-    print("metadata_size", metadata_size)
-    print("metadata_size", int(metadata_size) / (16*1024) )
-    print("metadata_size rounds", math.ceil(int(metadata_size) / (16*1024)) )
-
-    
-
 except socket.timeout:
     print((IP, PORT),'timeout !')
 
 
-# print("")
+print("\n",'-' * 21,"REQUEST PIECE",'-' * 21,"\n")
 
 
-# msg = chr(BT_MSG_ID).encode() + chr(EXT_HANDSHAKE).encode() + bencode.bencode(bencode.bencode({'msg_type': 0, 'piece': 0})).encode() 
-# msgLen = struct.pack(">I",len(msg))
-# msg = msgLen+msg
-# print(">>>", msg)
-# CLIENT_SOCKET.send(msg)
+# get metadata_size
+regex =  re.compile(r"metadata_sizei(\d+)e")
+matches = regex.search(server_response.decode("ascii", "ignore"))
+metadata_size = matches.group(1)
+# print("metadata_size", metadata_size)
+# print("metadata_size", int(metadata_size) / (16*1024) )
+ut_metadata_pieces = math.ceil(int(metadata_size) / (16*1024)) # The metadata is handled in blocks of 16KiB = 16384 Bytes = 16*1024
+print("metadata_size rounds:", ut_metadata_pieces)
 
-# try:
-#     server_response = CLIENT_SOCKET.recv(BUFFER)
-#     print("<<<",server_response)
-# except socket.timeout:
-#     print((IP, PORT),'timeout !')
+extended_message_id = 20
+for metadata_piece in range(ut_metadata_pieces + 1):
+    # msg = struct.pack("B", BITTORRENT_MESSAGE_ID) # struct.pack("B",20)
+    msg = struct.pack("B", extended_message_id) # struct.pack("B",0)
+    msg += bencode.bencode({'msg_type': 0, 'piece': metadata_piece}).encode()
+
+    LENGTH_PREFIX = struct.pack(">I",len(msg)) # len(msg).to_bytes(4, 'big') # struct.pack(">I",len(msg))
+    print("len",len(msg))
+    msg = LENGTH_PREFIX+msg
+    print(">>>", msg)
+    CLIENT_SOCKET.send(msg)
+    try:
+        metadata = CLIENT_SOCKET.recv(BUFFER)
+        print("<<< M", metadata)
+    except socket.timeout:
+        print((IP, PORT),'msg_type 0: timeout !')
+        break
 
 CLIENT_SOCKET.close()
